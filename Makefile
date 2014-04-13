0c9d4f507a2d9692daccb3a145c5f417edad48b3
Stephen Blott <smblott+github@gmail.com>
2014-04-13 17:11:04 +0100

    Better installation script.

--- Makefile
+++ Makefile
@@ -1,18 +1,12 @@
 
-default:
-	@echo No default. Read the Makefile.
-
-run:
-	python ./podrecast
-
-cgi:
-	REQUEST_METHOD=GET PATH_INFO=/Cycling/Velo-Cast HTTP_HOST=localhost SCRIPT_NAME=/cgi-bin/podrecast python ./podrecast
-
-index:
-	REQUEST_METHOD=GET PATH_INFO=/ HTTP_HOST=localhost SCRIPT_NAME=/cgi-bin/podrecast python ./podrecast
+# Default CGI directory.  To choose another directory, use:
+#
+#    sudo make cgi=/PATH/TO/CGI install
+#
+cgi = /usr/lib/cgi-bin
 
 install:
-	python setup.py install
+	CGI_DIR=$(cgi) python setup.py install
 
 build:
 	python setup.py build
@@ -20,5 +14,8 @@ build:
 dist:
 	python setup.py sdist
 
-.PHONY: default run cgi index install build dist
+clean:
+	rm -fr build dist 
+
+.PHONY: install build dist clean
  
--- setup.py
+++ setup.py
@@ -1,16 +1,62 @@
 #!/usr/bin/env python
 
 import sys,os
+from subprocess import call
 from distutils.core import setup
+from distutils.command.install import install as Install
 
-cgi  = []
-cgis = [ '/usr/lib/cgi-bin', '/usr/lib/cgi', '/var/www/cgi-bin', '/var/www/cgi' ]
+# ####################################################################
+# Custom installer...
 
-for directory in cgis:
-   if os.path.isdir(directory):
-      print 'choosing cgi directory', directory
-      cgi = [ ( directory, [ 'podrecast' ] ) ]
-      break
+class Installer(Install):
+   cgi_dir = 'CGI_DIR'
+
+   def run(self):
+      Install.run(self)
+      #
+      cgi = self.find_cgi()
+      if cgi and self.install_cgi(cgi):
+         return
+      else:
+         print
+         print "WARNING:"
+         print "   Could not find the cgi directory."
+         print "   Everything has been installed *except* for the cgi script."
+         print "   Please install it manually, or set a CGI_DIR"
+         print "   environment variable. Try one of..."
+         print
+         print "      sudo make cgi=/PATH/TO/CGI install"
+         print "      sudo CGI_DIR=/PATH/TO/CGI python setup.py"
+
+   def find_cgi(self):
+      #
+      if self.cgi_dir not in os.environ:
+         print "CMD_DIR not set"
+         return None
+      #
+      cgi = os.environ[self.cgi_dir]
+      if not os.path.isdir(cgi):
+         print "directory does not exist:", cgi
+         return None
+      #
+      return cgi
+
+   def install_cgi(self,cgi):
+      src = 'podrecast'
+      dst = "{}/{}".format(cgi,src)
+      print "choosing {} as the cgi directory".format(cgi)
+      print "installing {} as {}".format(src,dst)
+      if 0 == call("install -v -m 0555".split() + [src, dst]):
+         print "cgi script installed successfully in", dst
+         return True
+      #
+      print
+      print "ERROR:"
+      print "   Failed to install."
+      return False
+
+# ####################################################################
+# Configuration...
 
 setup(name='podrecast',
       author='Stephen Blott',
@@ -22,20 +68,7 @@ setup(name='podrecast',
       long_description=open('README.txt').read(),
       platforms=['Unix'],
       scripts=['podrecast', 'script/podrecast_mp3gain', 'script/podrecast_simple' ],
-      data_files=[ ( '/usr/local/etc', [ 'sample/podrecast.conf.sample' ] ), ] + cgi,
+      data_files=[ ( '/usr/local/etc', [ 'sample/podrecast.conf.sample' ] ) ],
+      cmdclass={'install': Installer},
       )
-
-if 'install' in sys.argv:
-   print
-   if cgi:
-      directory, [ scriptname ] = cgi[0]
-      script = "{}/{}".format(directory,scriptname)
-      print 'changing mode of {} to 0555'.format(script)
-      os.chmod(script,0o555)
-      print "CGI script successfully installed in", script
-      print "please verify that this is the correct CGI directory"
-   else:
-      print "WARNING: Everything installed *except* for the cgi script."
-      print "         Could not find the CGI directory."
-      print "         Please install it (podrecast) manually."
 

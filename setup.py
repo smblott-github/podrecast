115b0356edd6e34b5f53baf284001a704b622d6e
Stephen Blott <smblott+github@gmail.com>
2014-04-23 15:17:27 +0100

    Rework installation and documentation.

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
 
+import sys, os, subprocess
 from distutils.core import setup
+from distutils.command.install import install as Install
+
+# ####################################################################
+# Custom installer...
+
+class Installer(Install):
+   cgi = [ '/usr/lib/cgi-bin', '~/public_html/cgi-bin' ]
+
+   def run(self):
+      Install.run(self)
+      for directory in self.cgi:
+         if self.cgi_install(directory):
+            return
+      print 'CGI bin not found.'
+      print 'CGI script (./podrecast) not installed; please install it by manually.'
+
+   def cgi_install(self,directory):
+      directory = os.path.expanduser(directory)
+      if os.path.isdir(directory) and os.access(directory, os.W_OK):
+         if 0 == subprocess.call('install -m 0555 podrecast'.split() + [ directory ]):
+            print 'cgi script installed in', directory
+            return True
+      return False
+
+# ####################################################################
+# Configuration...
 
 setup(name='podrecast',
       author='Stephen Blott',
@@ -13,5 +40,6 @@ setup(name='podrecast',
       platforms=['Unix'],
       scripts=['podrecast', 'script/podrecast_mp3gain', 'script/podrecast_simple' ],
       data_files=[ ( 'etc', [ 'sample/podrecast.conf.sample' ] ) ],
+      cmdclass={'install': Installer},
       )
 

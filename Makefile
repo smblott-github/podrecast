a1e628c5a44794981c2e95ab5abf420ab8407f5d
Stephen Blott <smblott+github@gmail.com>
2014-04-23 13:56:30 +0100

    Better install instructions.

--- Makefile
+++ Makefile
@@ -1,16 +1,10 @@
 
-# Default CGI directory.  To choose another directory, use:
-#
-#    sudo make cgi=/PATH/TO/CGI/DIRECTORY install
-#
-cgi = /usr/lib/cgi-bin
-
 build:
 	python setup.py build
 
 install:
 	$(MAKE) build
-	sudo CGI_DIR=$(cgi) python setup.py install
+	sudo python setup.py install
 
 dist:
 	python setup.py sdist
@@ -25,5 +19,5 @@ upload:
 	$(MAKE) publish
 
 
-.PHONY: install build dist clean publish upload
+.PHONY: build install dist clean publish upload
  
--- setup.py
+++ setup.py
@@ -1,62 +1,6 @@
 #!/usr/bin/env python
 
-import sys,os
-from subprocess import call
 from distutils.core import setup
-from distutils.command.install import install as Install
-
-# ####################################################################
-# Custom installer...
-
-class Installer(Install):
-   cgi_dir = 'CGI_DIR'
-
-   def run(self):
-      Install.run(self)
-      #
-      cgi = self.find_cgi()
-      if cgi and self.install_cgi(cgi):
-         return
-      else:
-         print
-         print "WARNING:"
-         print "   Could not find the cgi directory."
-         print "   Everything has been installed *except* for the cgi script."
-         print "   Please install run 'make install' as follows"
-         print
-         print "      make cgi=/PATH/TO/CGI/DIRECTORY install"
-         print
-         sys.exit(1)
-
-   def find_cgi(self):
-      #
-      if self.cgi_dir not in os.environ:
-         print "{} is not set".format(self.cgi_dir)
-         return None
-      #
-      cgi = os.environ[self.cgi_dir]
-      if not os.path.isdir(cgi):
-         print "directory does not exist:", cgi
-         return None
-      #
-      return cgi
-
-   def install_cgi(self,cgi):
-      src = 'podrecast'
-      dst = "{}/{}".format(cgi,src)
-      print "choosing {} as the cgi directory".format(cgi)
-      print "installing {} as {}".format(src,dst)
-      if 0 == call("install -v -m 0555".split() + [src, dst]):
-         print "cgi script installed successfully in", dst
-         return True
-      #
-      print
-      print "ERROR:"
-      print "   Failed to install."
-      return False
-
-# ####################################################################
-# Configuration...
 
 setup(name='podrecast',
       author='Stephen Blott',
@@ -68,7 +12,6 @@ setup(name='podrecast',
       long_description=open('README.txt').read(),
       platforms=['Unix'],
       scripts=['podrecast', 'script/podrecast_mp3gain', 'script/podrecast_simple' ],
-      data_files=[ ( '/usr/local/etc', [ 'sample/podrecast.conf.sample' ] ) ],
-      cmdclass={'install': Installer},
+      data_files=[ ( 'etc', [ 'sample/podrecast.conf.sample' ] ) ],
       )
 
